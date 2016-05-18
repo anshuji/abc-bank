dcede89ec3087ba696db8bf5081af6cf4fed72da
anshuji <rajain.anshu86@gmail.com>
2016-05-18 11:37:53 -0400

    edited transactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -4,9 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+@RunWith(PowerMockRunner.class)
 public class TransactionTest {
+	 @Before 
+	 public void setUp() {
+      PowerMockito.mockStatic(DateProvider.class);
+      PowerMockito.when(DateProvider.getInstance().thenReturn(new Date());
+   }
+	
     @Test
     public void transaction() {
+		// we need to use mockito(testNG)/powerMock(JUnit) as DateProvider has a static method , we will mock it
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
     }
